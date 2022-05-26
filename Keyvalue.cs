006bf88a4b2db4c44d3aaf47a5e84babe64259ed
Trần Anh Hào <85891846+haojames@users.noreply.github.com>
2022-05-26 14:08:40 +0700

    Create Keyvalue.cs

---
+++ Keyvalue.cs
@@ -0,0 +1,16 @@
+RequestResponseReg = 'RequestResponse\((.*)[ ]*,[ ]*(.*)[ ]*,[ ]*(.*)\)'
+COL_TC_ID = "ID.?"
+COL_component_name = "XXX Component MDC Component COM Tests MDC DCOM Tests LabT_MDC_ERRMGR MDC Error Manager Tests  LabT_MDC_DCOM MDC_ERRMGR Labor Test Lane Deviation Warning"
+COL_test_description = "Test Description.?"
+COL_test_Step = "Test.*Steps.?"
+COL_test_response = "Test response.?"
+COL_teststep_keywords = "Test.+?words"
+COL_objectType = "ObjectType.?"
+COL_teststatus = "TestStatus.?"
+COL_project = "Project.?"
+# todo --> modify input from gui
+COL_release = ""
+COL_test_result = ""
+counter = 0
+log_handle = logging.getLogger('ToolHuan')
+glb_progress = None
